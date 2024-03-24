0ad438d0a188b84627924ec37d0e8de1a9008d11
Dipendra Nath <dipen.2052@gmail.com>
2024-03-24 17:50:54 -0400

    product migration created

---
+++ Models/Comment.cs
@@ -0,0 +1,7 @@
+namespace Assignment3.Models {
+    public class Comment {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int ProductId { get; set; }
+    }
+}
